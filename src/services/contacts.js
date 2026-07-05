50f94d3def18efe8b468a4696748f2e4c09c1731
omer <ofaruk.yildirim@outlook.com>
2026-07-05 15:46:41 +0300

    validation-final

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,36 @@
 import { Contact } from '../db/models/Contact.js';
 
-export const getAllContacts = async () => {
-  return Contact.find();
-};
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortBy = 'name',
+  sortOrder = SORT_ORDER.ASC,
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+
+  const contactsCount = await Contact.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
 export const getContactsById = async (id) => {
   return Contact.findById(id);
 };
