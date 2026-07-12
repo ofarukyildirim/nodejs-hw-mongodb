788aef4a90d4a71596828a4cb8ad022c52d63107
omer <ofaruk.yildirim@outlook.com>
2026-07-12 20:07:32 +0300

    hw6-final

---
+++ src/utils/sendMail.js
@@ -0,0 +1,15 @@
+import nodemailer from 'nodemailer';
+import { env } from './env.js';
+
+const transporter = nodemailer.createTransport({
+  host: env('SMTP_HOST'),
+  port: Number(env('SMTP_PORT')),
+  auth: {
+    user: env('SMTP_USER'),
+    pass: env('SMTP_PASSWORD'),
+  },
+});
+
+export const sendMail = async (options) => {
+  return transporter.sendMail(options);
+};
